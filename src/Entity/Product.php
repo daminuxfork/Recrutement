845f4586d93e8787d296c5ed5fca7d60208526f2
alikhyati <alikhyatihotmail.com>
2022-02-08 15:20:49 +0100

    exercice2

---
+++ src/Entity/Product.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $priceHt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creationDate;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateUpdate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPriceHt(): ?float
+    {
+        return $this->priceHt;
+    }
+
+    public function setPriceHt(float $priceHt): self
+    {
+        $this->priceHt = $priceHt;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getDateUpdate(): ?\DateTimeInterface
+    {
+        return $this->dateUpdate;
+    }
+
+    public function setDateUpdate(?\DateTimeInterface $dateUpdate): self
+    {
+        $this->dateUpdate = $dateUpdate;
+
+        return $this;
+    }
+
+}
--- src/Services/AlgorithmeService.php
+++ src/Services/AlgorithmeService.php
@@ -4,5 +4,23 @@ namespace App\Services;
 
 class AlgorithmeService
 {
-//    TODO: CODE HERE
+    public function algoFirst($integers)
+    {
+        $result = '';
+        if (count($integers) < 3){
+            $result = 'Invalid';
+        }
+    }
+
+    public function algoSecond($integers)
+    {
+        $filteredArray = [];
+
+        foreach($integers as $integer){
+            if ($integer <= 10 ){
+                $filteredArray[] = $integer;
+            }
+        }
+        var_dump($filteredArray);
+    }
 }
